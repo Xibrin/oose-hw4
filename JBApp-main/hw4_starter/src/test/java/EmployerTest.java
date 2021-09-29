48bc145459fbff1506df6207aa978526aa9d8f06
Xibrin <64810043+Xibrin@users.noreply.github.com>
2021-09-29 18:21:03 -0400

    Completed TODO 2

--- JBApp-main/hw4_starter/src/test/java/EmployerTest.java
+++ JBApp-main/hw4_starter/src/test/java/EmployerTest.java
@@ -313,9 +313,17 @@ public class EmployerTest {
         public void setUpAll() {
             client = new OkHttpClient();
         }
-
+        
         @Test
         public void testHTTPGetEmployersEndpoint() throws IOException {
+            String endpoint = BASE_URL + "/employers";
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(endpoint)
+                    .build();
+            Response response = client.newCall(request).execute();
+
+            assertEquals(response.code(),200);
             // TODO 2: Write code to send a http get request using OkHttp to thr
             //  "employers" endpoint and assert that the received status code is OK (200)!
             //  Note: In order for this to work, you need to make sure your local sparkjava
